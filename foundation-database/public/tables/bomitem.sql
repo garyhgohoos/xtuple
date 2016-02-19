72da2252e87e6b726ed9c21122595ea99b56c692
garyhgohoos <gary@xtuple.com>
2016-02-19 09:36:35 -0500

    Issue #27477:preliminary commit

---
+++ foundation-database/public/tables/bomitem.sql
@@ -0,0 +1,2 @@
+select xt.add_column('bomitem', 'bomitem_created',      'TIMESTAMP WITH TIME ZONE', NULL, 'public');
+select xt.add_column('bomitem', 'bomitem_last_modified','TIMESTAMP WITH TIME ZONE', NULL, 'public');
