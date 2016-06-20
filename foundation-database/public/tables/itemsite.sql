a3af95190d58ba7d6f5f48587d67ad34f00cf6db
garyhgohoos <gary@xtuple.com>
2016-06-20 17:22:02 -0400

    Issue #27477:react to comments

--- foundation-database/public/tables/itemsite.sql
+++ foundation-database/public/tables/itemsite.sql
@@ -1,3 +1,7 @@
+
+select xt.add_column('itemsite', 'itemsite_created',      'TIMESTAMP WITH TIME ZONE', NULL, 'public');
+select xt.add_column('itemsite', 'itemsite_lastupdated',  'TIMESTAMP WITH TIME ZONE', NULL, 'public');
+
 -- incident 23507:change how qoh, qoh available, and qoh netable are determined
 do $$
 begin
@@ -6,6 +10,3 @@ if (compareVersion(fetchMetricText('ServerVersion'), '4.7.0') = -1) then
   alter table itemsite drop column itemsite_nnqoh cascade;
 end if;
 end$$;
-
-select xt.add_column('itemsite', 'itemsite_created',      'TIMESTAMP WITH TIME ZONE', NULL, 'public');
-select xt.add_column('itemsite', 'itemsite_lastupdated',  'TIMESTAMP WITH TIME ZONE', NULL, 'public');
--- foundation-database/public/trigger_functions/bomhead.sql
+++ foundation-database/public/trigger_functions/bomhead.sql
@@ -29,10 +29,6 @@ CREATE TRIGGER bomheadBeforeTrigger BEFORE INSERT OR UPDATE ON bomhead
 CREATE OR REPLACE FUNCTION _bomheadAfterTrigger() RETURNS TRIGGER AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECLARE
-  _revid INTEGER;
-  _check TEXT;
-  _cmnttypeid INTEGER;
 BEGIN
   -- Changelog
   IF (TG_OP = 'INSERT') THEN
